8df3ad4fbf673befd314156db60e382d2e2c15e5
PJ <33669966+PhillipJacobsen@users.noreply.github.com>
2020-05-03 21:46:48 -0600

    Update Radians_IoT_Faucet.ino

--- Radians_IoT_Faucet.ino
+++ Radians_IoT_Faucet.ino
@@ -6,7 +6,7 @@
     2020 @phillipjacobsen
 
     Program Features:
-    This program implements a Telegram BOT running on an ESP32 microcontroller that can dispense Ark.io blockchain tokens upon request.
+    This program implements a Telegram BOT running on an ESP32 microcontroller that can dispense Ark.io custom blockchain tokens upon request.
 
     FaucetAddress - address of your Radians bridgechain token faucet
     PAYOUT_MESSAGE - message you wish to add to each payout
